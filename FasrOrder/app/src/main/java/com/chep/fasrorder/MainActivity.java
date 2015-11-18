ac0eb496c84cf75affe5f79dbfde45df461152a0
Алексей Чепурко < >
2015-11-18 13:25:42 +0300

    added map layout

--- FasrOrder/app/src/main/java/com/chep/fasrorder/MainActivity.java
+++ FasrOrder/app/src/main/java/com/chep/fasrorder/MainActivity.java
@@ -1,9 +1,19 @@
 package com.chep.fasrorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void openMap(View view){
+         // TODO: 18.11.2015
+        // redirect To map layout
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
